ae231d8ca29798541baf0e65f99560b4948a679b
Daria <dashalokomoko30@gmail.com>
2024-07-22 09:21:59 -0400

    Fahrenheit to Celcius functionality

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,13 @@
-package app;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My converter");
+        System.out.println(" It can convert  between Fahrenheit and Celsius.");
+        double fahrenheit = 98.6;
+        double celsius = Main.fahrenheitToCelsius(fahrenheit);
+        System.out.println(fahrenheit + " Fahrenheit is " + celsius + " Celsius.");
+    }
+
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
